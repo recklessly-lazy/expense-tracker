2e6dad61dfe726ca8b250eaa310a9f9e7938942b
Ramalakshmanan.S <srlaxman1497@gmail.com>
2023-01-11 01:53:41 +0530

    form validations added

--- src/components/NewExpenseForm/NewExpenseForm.tsx
+++ src/components/NewExpenseForm/NewExpenseForm.tsx
@@ -20,24 +20,100 @@ function NewExpenseForm({
     const [expenseAmount, setExpenseAmount] = useState(0);
     const [expenseDate, setExpenseDate] = useState("");
 
+    const [expenseTitleErr, setExpenseTitleErr] = useState("");
+    const [expenseAmountErr, setExpenseAmountErr] = useState("");
+    const [expenseDateErr, setExpenseDateErr] = useState("");
+
+    const [expenseTitleTouched, setExpenseTitleTouched] = useState("");
+    const [expenseAmountTouched, setExpenseAmountTouched] = useState("");
+    const [expenseDateTouched, setExpenseDateTouched] = useState("");
+
     const dispatch = useAppDispatch();
 
     useEffect(() => {
         if (show) setRender(show);
     }, [show]);
 
+    function clearFormData() {
+        setExpenseTitle('');
+        setExpenseAmount(0);
+        setExpenseDate('');
+    }
+
+    function validateForm() {
+        console.log(
+            "title:",
+            expenseTitle === "",
+            ", amt:",
+            expenseAmount,
+            ", date:",
+            expenseDate
+        );
+        let isFormValid = true;
+        let isTitleEmpty = expenseTitle === "";
+        console.log("istitle ===", isTitleEmpty);
+
+        if (isTitleEmpty) {
+            console.log("istitle ===", isTitleEmpty);
+            setExpenseTitleErr((prev) => "Expense title cannot be empty !");
+            isFormValid = false;
+        } else {
+            setExpenseTitleErr("");
+        }
+        if (isNaN(expenseAmount) || expenseAmount === 0) {
+            setExpenseAmountErr("Expense amount can't be left empty");
+            isFormValid = false;
+        } else {
+            setExpenseAmountErr("");
+        }
+
+        if (!(expenseDate === "")) {
+            let date = new Date(expenseDate);
+            let now = new Date();
+            let diffDays =
+                (date.valueOf() - now.valueOf()) / (1000 * 60 * 60 * 24);
+
+            if (diffDays > 0) {
+                setExpenseDateErr("Date can't be in the future !");
+                isFormValid = false;
+            } else {
+                setExpenseDateErr("");
+            }
+        } else {
+            setExpenseDateErr("Date can't be empty");
+            isFormValid = false;
+        }
+
+        console.log(
+            "expenseTitleErr:",
+            expenseTitleErr,
+            ", expenseAmountErr",
+            expenseAmountErr,
+            ", expenseDateErr",
+            expenseDateErr
+        );
+        return isFormValid;
+    }
     function addExpenseHandler(event: FormEvent) {
         event.preventDefault();
-        let newExpense: Expense = {
-            id: uuidv4(),
-            amount: expenseAmount,
-            title: expenseTitle,
-            date: expenseDate,
-        };
-        console.log("newExp = ", newExpense);
-        console.log("date ===", new Date(newExpense.date));
-        dispatch(addExpense(newExpense));
-        onExpenseAdd();
+        let newExpense: Expense;
+        let isValid = validateForm();
+        console.log("is form valid ?", isValid);
+        if (isValid) {
+            newExpense = {
+                id: uuidv4(),
+                amount: expenseAmount,
+                title: expenseTitle,
+                date: expenseDate,
+            };
+            console.log("newExp = ", newExpense);
+            console.log("date ===", new Date(newExpense.date));
+            dispatch(addExpense(newExpense));
+            onExpenseAdd();
+            clearFormData();
+        } else {
+            return;
+        }
     }
     const classes = [styles.form, show ? styles.entry : styles.exit].join(" ");
     console.log("classes = ", classes);
@@ -52,7 +128,10 @@ function NewExpenseForm({
                 }}
             >
                 <div className={styles.formHeader}>New Expense</div>
-                <form className={"pb-2"+' ' + styles.formBody} onSubmit={addExpenseHandler}>
+                <form
+                    className={"pb-2" + " " + styles.formBody}
+                    onSubmit={addExpenseHandler}
+                >
                     <div className="pb-3">
                         <label className="form-label" htmlFor="expense_title">
                             Expense title
@@ -65,6 +144,7 @@ function NewExpenseForm({
                             placeholder="expense title"
                             onChange={(e) => setExpenseTitle(e.target.value)}
                         />
+                        <div className="text-danger">{expenseTitleErr}</div>
                     </div>
                     <div className="pb-3">
                         <label className="form-label" htmlFor="expense_amount">
@@ -80,6 +160,7 @@ function NewExpenseForm({
                                 setExpenseAmount(parseFloat(e.target.value))
                             }
                         />
+                        <div className="text-danger">{expenseAmountErr}</div>
                     </div>
                     <div className="pb-3">
                         <label className="form-label" htmlFor="expense_amount">
@@ -92,6 +173,7 @@ function NewExpenseForm({
                             name="expense_date"
                             onChange={(e) => setExpenseDate(e.target.value)}
                         />
+                        <div className="text-danger">{expenseDateErr}</div>
                     </div>
                     <button type="submit" className="btn btn-primary">
                         Add
