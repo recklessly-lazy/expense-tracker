7abf8fbdd70cc2b664ae525b1cfe0bc008d5f2d3
Ramalakshmanan.S <srlaxman1497@gmail.com>
2023-01-23 22:02:04 +0530

    float button draggable

--- src/pages/ExpensePage/ExpensePage.tsx
+++ src/pages/ExpensePage/ExpensePage.tsx
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./ExpensePage.module.scss";
 import ExpensesList from "../../components/ExpensesList/ExpensesList";
 import NewExpenseForm from "../../components/NewExpenseForm/NewExpenseForm";
-
+import { motion } from "framer-motion";
 function ExpensePage() {
     const [showExpenseform, setShowExpenseform] = useState(false);
     const toggleAddExpenseForm = () => {
         setShowExpenseform((prevState) => !prevState);
     };
+    const nodeRef = useRef(null);
+    const [dragging, setDragging] = useState(false);
     return (
-        <div className={styles.expensePage}>
+        <div className={styles.expensePage} ref={nodeRef}>
             <ExpensesList />
             <NewExpenseForm
                 onExpenseAdd={() => {
@@ -17,13 +19,36 @@ function ExpensePage() {
                 }}
                 show={showExpenseform}
             />
-            <button className={styles.addBtn} onClick={toggleAddExpenseForm}>
+            <motion.button
+                className={styles.addBtn}
+                onClick={
+                    !dragging
+                        ? toggleAddExpenseForm
+                        : () => {
+                              setDragging(false);
+                          }
+                }
+                drag
+                dragConstraints={nodeRef}
+                onDragStart={() => {
+                    setDragging(true);
+                    console.log("dragging start");
+                }}
+                dragTransition={{
+                    min: 0,
+                    max: 100,
+                    bounceDamping: 20,
+                    bounceStiffness: 500,
+                    power: 0.2,
+                    
+                }}
+            >
                 {!showExpenseform ? (
                     <i className="bi bi-plus" style={{ fontSize: "2rem" }}></i>
                 ) : (
                     <i className="bi bi-x" style={{ fontSize: "2rem" }}></i>
                 )}
-            </button>
+            </motion.button>
         </div>
     );
 }
